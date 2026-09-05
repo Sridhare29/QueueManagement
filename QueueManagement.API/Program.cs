bad0ec06c4901ac38db3878d4c8b17a5fa9ab25f
Sridhar Elumalai <sridhar.elumalai@kanini.com>
2026-09-05 19:57:52 +0530

    add CQRS

--- QueueManagement.API/Program.cs
+++ QueueManagement.API/Program.cs
@@ -7,6 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -18,6 +28,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors("FrontendPolicy");
 app.UseSwagger();
 app.UseSwaggerUI();
 
